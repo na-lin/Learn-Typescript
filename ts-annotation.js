8af886e34244d176ea0c83d6334573199813fbdd
Na Lin <lna483018@gmail.com>
2023-02-25 15:59:15 -0500

    type annotations

---
+++ ts-annotation.js
@@ -0,0 +1,7 @@
+var movieTitle = "Amadeus";
+movieTitle = "Arrival";
+movieTitle = 123;
+movieTitle.upper();
+movieTitle.toLowerCase();
+// Number Type
+var numOfCats = 9;
---
+++ ts-annotation.ts
@@ -0,0 +1,24 @@
+let movieTitle: string = "Amadeus";
+movieTitle = "Arrival";
+movieTitle = 123;
+movieTitle.upper();
+movieTitle.toLowerCase();
+
+// Number Type
+let numOfCats: number = 9;
+
+// inference
+let test = 9;
+test = "hello";
+
+// any
+let numOfLe: any;
+numOfLe = "hellp";
+numOfLe.uppers();
+
+// delayed init
+let found;
+found = "hello"; // still any
+
+let foundMovie: string;
+foundMovie = 12;
