599ba1625be540f9fcfcb0db8a81883d35a01d4d
Na Lin <lna483018@gmail.com>
2023-03-17 13:51:57 -0400

    public and private, shorthand

--- class/dist/index.js
+++ class/dist/index.js
@@ -1,13 +1,30 @@
 "use strict";
+// class Player {
+//   // this class has properties with this type
+//   public readonly first: string; //!Cannot assign to 'first' because it is a read-only property
+//   public readonly last: string;
+//   // public field
+//   private score: number = 0;
+//   // !Parameter 'first' implicitly has an 'any' type.
+//   // !Property 'first' does not exist on type 'Player'. -> need to tell the type of first before assign
+//   constructor(first: string, last: string) {
+//     this.first = first;
+//     this.last = last;
+//   }
+//   private secret(): void {
+//     console.log("secret");
+//   }
+// }
 class Player {
-    // !Parameter 'first' implicitly has an 'any' type.
-    // !Property 'first' does not exist on type 'Player'. -> need to tell the type of first before assign
-    constructor(first, last) {
-        // public field
-        this.score = 0;
+    // private score: number = 0;
+    constructor(first, last, score = 0) {
         this.first = first;
         this.last = last;
+        this.score = score;
+    }
+    secret() {
+        console.log("secret");
     }
 }
 const player1 = new Player("john", "excel");
-player1.first = "12";
+console.log(player1);
--- class/src/index.ts
+++ class/src/index.ts
@@ -1,17 +1,35 @@
-class Player {
-  // this class has properties with this type
-  readonly first: string; //!Cannot assign to 'first' because it is a read-only property
-  readonly last: string;
-  // public field
-  score: number = 0;
+// class Player {
+//   // this class has properties with this type
+//   public readonly first: string; //!Cannot assign to 'first' because it is a read-only property
+//   public readonly last: string;
+//   // public field
+//   private score: number = 0;
 
-  // !Parameter 'first' implicitly has an 'any' type.
-  // !Property 'first' does not exist on type 'Player'. -> need to tell the type of first before assign
-  constructor(first: string, last: string) {
-    this.first = first;
-    this.last = last;
+//   // !Parameter 'first' implicitly has an 'any' type.
+//   // !Property 'first' does not exist on type 'Player'. -> need to tell the type of first before assign
+//   constructor(first: string, last: string) {
+//     this.first = first;
+//     this.last = last;
+//   }
+
+//   private secret(): void {
+//     console.log("secret");
+//   }
+// }
+
+class Player {
+  // private score: number = 0;
+
+  constructor(
+    public first: string,
+    public last: string,
+    private score: number = 0
+  ) {}
+
+  private secret(): void {
+    console.log("secret");
   }
 }
 
 const player1 = new Player("john", "excel");
-player1.first = "12";
+console.log(player1);
