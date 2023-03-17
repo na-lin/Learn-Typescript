a6c707fe059e804fdd385f8d6da4627e37ef0459
Na Lin <lna483018@gmail.com>
2023-03-17 00:12:35 -0400

    type assertion with DOM

--- mini-project/dist/index.js
+++ mini-project/dist/index.js
@@ -3,13 +3,18 @@
 document; // type with Document
 // type of btn: HTMLElement | null
 const btn = document.getElementById("btn");
+// const input = document.getElementById("todoInput");
+const input = document.getElementById("todoInput");
 // Approach 2: non-null assertion operator ! -> this guarentee not to be null
 // const btn = document.getElementById("btn")!;
 // Approach 1: ?. -> if btn exist, which mean btn !== null, add event listner to the element
 btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", () => {
     console.log("click");
+    console.log(input.value); // Property 'value' does not exist on type 'HTMLElement'
+    // Tell typescript input is a HTMLInputElement, which have the value property
 });
 // ##### Type Assertions, I know more then you
 let mystery = "Hello world";
 // don't change the type of mystery, but just there treat mystery with type of string
 const numChars = mystery.length;
+// ##### Type Assertions with DOM
--- mini-project/src/index.ts
+++ mini-project/src/index.ts
@@ -2,7 +2,9 @@
 document; // type with Document
 
 // type of btn: HTMLElement | null
-const btn = document.getElementById("btn");
+const btn = document.getElementById("btn") as HTMLButtonElement;
+// const input = document.getElementById("todoInput");
+const input = document.getElementById("todoInput") as HTMLInputElement;
 
 // Approach 2: non-null assertion operator ! -> this guarentee not to be null
 // const btn = document.getElementById("btn")!;
@@ -10,9 +12,13 @@ const btn = document.getElementById("btn");
 // Approach 1: ?. -> if btn exist, which mean btn !== null, add event listner to the element
 btn?.addEventListener("click", () => {
   console.log("click");
+  console.log(input.value); // Property 'value' does not exist on type 'HTMLElement'
+  // Tell typescript input is a HTMLInputElement, which have the value property
 });
 
 // ##### Type Assertions, I know more then you
 let mystery: unknown = "Hello world";
 // don't change the type of mystery, but just there treat mystery with type of string
 const numChars = (mystery as string).length;
+
+// ##### Type Assertions with DOM
