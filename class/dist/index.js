94218e7d471cafa64fdbf3def94d9db0e6e36fcf
Na Lin <lna483018@gmail.com>
2023-03-17 13:09:18 -0400

    private fields

--- class/dist/index.js
+++ class/dist/index.js
@@ -1,6 +1,6 @@
 class Player {
   // add value which is hard code to init
-  score = 0;
+  #score = 0;
   numLives = 10;
 
   // dynamic init value
@@ -14,6 +14,18 @@ class Player {
   loseLive() {
     this.numLives -= 1;
   }
+  getScore() {
+    return this.#score;
+  }
+  setScore(newScore) {
+    this.#score = newScore;
+  }
+
+  #secret() {
+    console.log("this is secret inside class");
+  }
 }
 
 const player1 = new Player("x", "y");
+// console.log(player1.#score);
+console.log(player1.getScore());
