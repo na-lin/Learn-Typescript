d6e19d337853aadfb1d8f4d65c5b54c44c86fc44
Na Lin <lna483018@gmail.com>
2023-03-17 00:21:30 -0400

    working with event

--- mini-project/src/index.ts
+++ mini-project/src/index.ts
@@ -1,24 +1,18 @@
-//  ### type of DOM
-document; // type with Document
-
-// type of btn: HTMLElement | null
 const btn = document.getElementById("btn") as HTMLButtonElement;
-// const input = document.getElementById("todoInput");
 const input = document.getElementById("todoInput") as HTMLInputElement;
+const form = document.querySelector("#todoform")!;
 
-// Approach 2: non-null assertion operator ! -> this guarentee not to be null
-// const btn = document.getElementById("btn")!;
+// btn?.addEventListener("click", () => {
+//   console.log("click");
+//   console.log(input.value);
+// });
 
-// Approach 1: ?. -> if btn exist, which mean btn !== null, add event listner to the element
-btn?.addEventListener("click", () => {
-  console.log("click");
-  console.log(input.value); // Property 'value' does not exist on type 'HTMLElement'
-  // Tell typescript input is a HTMLInputElement, which have the value property
+function handleSubmit(e: SubmitEvent) {
+  e.preventDefault();
+  console.log("submit");
+}
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("submit");
 });
-
-// ##### Type Assertions, I know more then you
-let mystery: unknown = "Hello world";
-// don't change the type of mystery, but just there treat mystery with type of string
-const numChars = (mystery as string).length;
-
-// ##### Type Assertions with DOM
