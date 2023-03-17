746afaf56daeba92ed39029e64e076c69a49e55f
Na Lin <lna483018@gmail.com>
2023-03-17 00:33:02 -0400

    build todo app

--- mini-project/dist/index.js
+++ mini-project/dist/index.js
@@ -2,15 +2,21 @@
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoInput");
 const form = document.querySelector("#todoform");
+const todoList = document.getElementById("todolist");
 // btn?.addEventListener("click", () => {
 //   console.log("click");
 //   console.log(input.value);
 // });
 function handleSubmit(e) {
     e.preventDefault();
-    console.log("submit");
+    const newTodoText = input.value;
+    const newLI = document.createElement("li");
+    newLI.textContent = newTodoText;
+    // checkbox
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    newLI.append(checkBox);
+    todoList === null || todoList === void 0 ? void 0 : todoList.append(newLI);
+    input.value = "";
 }
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    console.log("submit");
-});
+form.addEventListener("submit", handleSubmit);
--- mini-project/src/index.ts
+++ mini-project/src/index.ts
@@ -1,18 +1,25 @@
 const btn = document.getElementById("btn") as HTMLButtonElement;
 const input = document.getElementById("todoInput") as HTMLInputElement;
 const form = document.querySelector("#todoform")!;
+const todoList = document.getElementById("todolist");
 
 // btn?.addEventListener("click", () => {
 //   console.log("click");
 //   console.log(input.value);
 // });
 
-function handleSubmit(e: SubmitEvent) {
+function handleSubmit(e: Event) {
   e.preventDefault();
-  console.log("submit");
+  const newTodoText = input.value;
+  const newLI = document.createElement("li");
+  newLI.textContent = newTodoText;
+
+  // checkbox
+  const checkBox = document.createElement("input");
+  checkBox.type = "checkbox";
+  newLI.append(checkBox);
+  todoList?.append(newLI);
+  input.value = "";
 }
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  console.log("submit");
-});
+form.addEventListener("submit", handleSubmit);
