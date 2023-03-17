b378cf48ded1411bdd106aaa4d1a945c3cdbfc2b
Na Lin <lna483018@gmail.com>
2023-03-17 00:04:37 -0400

    the type of DOM

--- mini-project/dist/index.js
+++ mini-project/dist/index.js
@@ -1,3 +1,11 @@
 "use strict";
+//  ### type of DOM
 document; // type with Document
-"hello".replaceAll("l", "!");
+// type of btn: HTMLElement | null
+const btn = document.getElementById("btn");
+// Approach 2: non-null assertion operator ! -> this guarentee not to be null
+// const btn = document.getElementById("btn")!;
+// Approach 1: ?. -> if btn exist, which mean btn !== null, add event listner to the element
+btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", () => {
+    console.log("click");
+});
--- mini-project/src/index.ts
+++ mini-project/src/index.ts
@@ -1,2 +1,13 @@
+//  ### type of DOM
 document; // type with Document
-"hello".replaceAll("l", "!");
+
+// type of btn: HTMLElement | null
+const btn = document.getElementById("btn");
+
+// Approach 2: non-null assertion operator ! -> this guarentee not to be null
+// const btn = document.getElementById("btn")!;
+
+// Approach 1: ?. -> if btn exist, which mean btn !== null, add event listner to the element
+btn?.addEventListener("click", () => {
+  console.log("click");
+});
