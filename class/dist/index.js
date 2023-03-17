6f7a9a54541d41dc08fa66063c125b1336d2c861
Na Lin <lna483018@gmail.com>
2023-03-17 13:04:17 -0400

    class field

--- class/dist/index.js
+++ class/dist/index.js
@@ -1,10 +1,18 @@
 class Player {
+  // add value which is hard code to init
+  score = 0;
+  numLives = 10;
+
+  // dynamic init value
   constructor(first, last) {
     this.first = first;
     this.last = last;
   }
   taught() {
     console.log("hello");
+  }
+  loseLive() {
+    this.numLives -= 1;
   }
 }
 
