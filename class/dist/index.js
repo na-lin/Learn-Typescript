5edbde9278bf2ba96af716803d5fe84ebd26c36a
Na Lin <lna483018@gmail.com>
2023-03-17 13:40:03 -0400

    class field and readonly

--- class/dist/index.js
+++ class/dist/index.js
@@ -1,10 +1,13 @@
 "use strict";
 class Player {
-    // Parameter 'first' implicitly has an 'any' type.
-    // Property 'first' does not exist on type 'Player'. -> need to tell the type of first before assign
+    // !Parameter 'first' implicitly has an 'any' type.
+    // !Property 'first' does not exist on type 'Player'. -> need to tell the type of first before assign
     constructor(first, last) {
+        // public field
+        this.score = 0;
         this.first = first;
         this.last = last;
     }
 }
 const player1 = new Player("john", "excel");
+player1.first = "12";
--- class/src/index.ts
+++ class/src/index.ts
@@ -1,9 +1,12 @@
 class Player {
   // this class has properties with this type
-  first: string;
-  last: string;
-  // Parameter 'first' implicitly has an 'any' type.
-  // Property 'first' does not exist on type 'Player'. -> need to tell the type of first before assign
+  readonly first: string; //!Cannot assign to 'first' because it is a read-only property
+  readonly last: string;
+  // public field
+  score: number = 0;
+
+  // !Parameter 'first' implicitly has an 'any' type.
+  // !Property 'first' does not exist on type 'Player'. -> need to tell the type of first before assign
   constructor(first: string, last: string) {
     this.first = first;
     this.last = last;
@@ -11,3 +14,4 @@ class Player {
 }
 
 const player1 = new Player("john", "excel");
+player1.first = "12";
