8cafa1440ee0df761f0800a0f7a5225eb023068d
Na Lin <lna483018@gmail.com>
2023-03-17 13:01:29 -0400

    constructor

--- class/dist/index.js
+++ class/dist/index.js
@@ -1,2 +1,11 @@
-"use strict";
-console.log("hellp");
+class Player {
+  constructor(first, last) {
+    this.first = first;
+    this.last = last;
+  }
+  taught() {
+    console.log("hello");
+  }
+}
+
+const player1 = new Player("x", "y");
