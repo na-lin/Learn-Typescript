329e90711297e2833d7357353430a55bc809feaa
Na Lin <lna483018@gmail.com>
2023-03-20 12:08:04 -0400

    truthiness guard

--- typeNarrow.ts
+++ typeNarrow.ts
@@ -4,3 +4,14 @@ function triple(value: number | string) {
   }
   return value.repeat(3);
 }
+
+function printLetter(word?: string) {
+  if (word) {
+    for (let char of word) {
+      console.log(char);
+    }
+  } else {
+    word;
+    console.log("You don't provide a word");
+  }
+}
