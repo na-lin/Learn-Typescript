1c7466ad101f2ff1e9698c2fd428676b891e991a
Na Lin <lna483018@gmail.com>
2023-03-25 00:14:54 -0400

    webpack production config

---
+++ ts-webpack/webpack.prod.js
@@ -0,0 +1,23 @@
+const path = require("path");
+module.exports = {
+  mode: "production",
+  entry: "./src/index.ts",
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", "js"],
+  },
+};
