34ac9947ee25a16321f284f184d007d876675be3
Na Lin <lna483018@gmail.com>
2023-03-25 18:10:30 -0400

    props passed from parent component

--- shopping-app/src/App.tsx
+++ shopping-app/src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Greeter from "./components/Greeter";
+import ShoppingList from "./components/ShoppingList";
 import "./App.css";
 
 function App() {
+  const items = [
+    { id: 1, product: "lemon", quantity: 3 },
+    { id: 2, product: "apply", quantity: 10 },
+  ];
   return (
-    <div className="App">
-      <Greeter person="Colt" />
+    <div>
+      <ShoppingList items={items} />
     </div>
   );
 }
