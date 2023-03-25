5117d1540b88edb76950333b7cd6bb10b5ed28cb
Na Lin <lna483018@gmail.com>
2023-03-25 18:17:27 -0400

    useState with ts

--- shopping-app/src/App.tsx
+++ shopping-app/src/App.tsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Greeter from "./components/Greeter";
 import ShoppingList from "./components/ShoppingList";
 import "./App.css";
 
+// models
+import Item from "./models/item";
+
 function App() {
-  const items = [
-    { id: 1, product: "lemon", quantity: 3 },
-    { id: 2, product: "apply", quantity: 10 },
-  ];
+  const [items, setItems] = useState<Item[]>([]);
+  // const items = [
+  //   { id: 1, product: "lemon", quantity: 3 },
+  //   { id: 2, product: "apply", quantity: 10 },
+  // ];
   return (
     <div>
       <ShoppingList items={items} />
